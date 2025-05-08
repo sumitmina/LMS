8563f43258c2bff4f1dfcd1c6f366c624ad2f00b
sumitmina <sumitmina2004@gmail.com>
2025-05-08 16:10:34 +0530

    cors error handled

--- server/src/app.js
+++ server/src/app.js
@@ -4,8 +4,7 @@ import cors from "cors"
 const app=express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: process.env.CORS_ORIGIN,  
 }))
 
 app.get("/",(req,res)=>{
