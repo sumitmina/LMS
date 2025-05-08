d7a7701d9000fb4604ff94e1d8d3a128c8178600
sumitmina <sumitmina2004@gmail.com>
2025-05-08 19:53:07 +0530

    changes in versel file

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,9 @@ import cors from "cors"
 
 const app=express()
 
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,  
-// }))
-
-app.use(cors())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,  
+}))
 
 app.get("/",(req,res)=>{
     res.send("API WORKING")
